f353b4a87cce294b1d8b853d5c51720832db8713
Juan Pablo Listte <juanpilistte@gmail.com>
2023-07-31 17:30:26 -0300

    Cambio de motor de bd a postgres

--- django-rest/inmuebles/inmuebles/settings.py
+++ django-rest/inmuebles/inmuebles/settings.py
@@ -81,10 +81,20 @@ AUTH_USER_MODEL = 'user_app.Account'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
+        'NAME': 'postgres',
+        'PASSWORD': 'machupichu1',
+        'HOST': 'localhost'
     }
 }
 
@@ -157,6 +167,9 @@ REST_FRAMEWORK = {
         'comentario-list': '800/day',
         'comentario-detail': '300/day',
     },
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',    
+    ),
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     # 'PAGE_SIZE':100,
 }
